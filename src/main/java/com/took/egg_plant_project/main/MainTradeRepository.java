5dda841391c1ee9a785ab9ac1da6ccf36a181817
ekdhs3423 <ekdhs3423@naver.com>
2025-07-25 16:34:27 +0900

    경로 및 문제있는부분 다듬음

--- src/main/java/com/took/egg_plant_project/main/MainTradeRepository.java
+++ src/main/java/com/took/egg_plant_project/main/MainTradeRepository.java
@@ -3,15 +3,18 @@ package com.took.egg_plant_project.main;
 import com.took.egg_plant_project.entity.Trade;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
+@Repository
 public interface MainTradeRepository extends JpaRepository<Trade, Integer> {
 
-    Optional<Trade> findByPostId(Integer postId);
+    Optional<Trade> findByPost_Id(Integer postId);
 
     @Modifying
     @Transactional
-    void deleteByPostId(Integer postId);
+    void deleteByPost_Id(Integer postId);
 }
+
